089a9484ffc22fb5126f7915401e8611898d1565
Sruthy <youremail@example.com>
2025-09-26 11:36:47 +0530

    Connected Firebase project

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,21 @@
-// firebase-config.js
-// Replace the object below with your firebase config from Firebase console
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyA7PYzkH8pm2EXCCn6EEW38_25vZ5IyBLc",
+  authDomain: "omkar-antiques-store.firebaseapp.com",
+  projectId: "omkar-antiques-store",
+  storageBucket: "omkar-antiques-store.firebasestorage.app",
+  messagingSenderId: "653253748520",
+  appId: "1:653253748520:web:3932a240494ed410e9921d",
+  measurementId: "G-T7ZK83EHK3"
 };
 
-export default firebaseConfig;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
